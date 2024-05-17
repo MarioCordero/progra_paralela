f917016d5e8ca92d65be1550a853598f83b3981d
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2024-05-17 16:49:21 -0600

    Cambios en el codigo, misma filosofía del commit anterior

--- paralela.Proyecto1/main.cpp
+++ paralela.Proyecto1/main.cpp
@@ -12,6 +12,12 @@ pthread_mutex_t mtx;
 // Definir el total de hilos a usar
 #define TOTAL_THREADS 5
 
+// Variable de condición para notificar a los consumidores
+pthread_cond_t cond_var;
+
+// Cola para almacenar las líneas leídas por entrada estandar
+queue<string> lineasSTD;
+
 // Se crea el struct para pasar el argumento a los hilos, el cual es principalmente la cola
 struct threadData {
     queue<string> *lineas;
@@ -19,6 +25,8 @@ struct threadData {
 
 
 // ---------------------------------------METODOS-------------------------------------------//
+
+// Metodo de imprimir
 void *printQueue(void *threadDataPointer){ // El parametro de esta funcion es un puntero generico, un puntero generico es un puntero que puede apuntar a cualquier tipo de dato
 
     // Recibir correctamente el struct y desreferenciarlo
@@ -26,19 +34,51 @@ void *printQueue(void *threadDataPointer){ // El parametro de esta funcion es un
     // Inicializar una cola con la direccion de memoria de la cola usada
     queue<string> *lineas = data->lineas;
 
-    //Lock
-    pthread_mutex_lock(&mtx);
 
-    if(!lineas->empty()) {
-        cout << lineas->front() << endl;
-        lineas->pop();
+    while (true) {
+        // Lock
+        pthread_mutex_lock(&mtx);
+
+        // Esperar hasta que haya datos en la cola
+        while (lineas->empty()) {
+            pthread_cond_wait(&cond_var, &mtx);
+        }
+
+        // Procesar una línea de la cola
+        if (!lineas->empty()) {
+            string line = lineas->front();
+            lineas->pop();
+            pthread_mutex_unlock(&mtx);
+            
+            // Procesar la línea fuera del bloqueo para permitir más concurrencia
+            cout << "Procesando línea: " << line << endl;
+        } else {
+            pthread_mutex_unlock(&mtx);
+        }
     }
 
-    //Unlock
-    pthread_mutex_unlock(&mtx);
-    
     pthread_exit(NULL);
 
+}
+
+// Metodo productor
+void productor() {
+
+    string linea;
+    cerr << "\n\tIngrese el texto. Ctrl+D para finalizar.\n" << endl;
+    while (getline(cin, linea)) {
+        // Lock
+        pthread_mutex_lock(&mtx);
+
+        // Encolar la línea leída por entrada estándar
+        lineasSTD.push(linea);
+
+        // Notificar a los consumidores que hay datos disponibles
+        pthread_cond_signal(&cond_var);
+
+        // Unlock
+        pthread_mutex_unlock(&mtx);
+    }
 }
 
 
@@ -51,53 +91,31 @@ int main() {
     // Inicialización de Mutex para evitar errores de sincronización
     pthread_mutex_init(&mtx, NULL);
 
-    // Cola para almacenar las líneas leídas por entrada estandar
-    queue<string> lineasSTD;
+    // Inicializar el struct con la direccion de memoria de la cola
+    threadData info;
 
-    // Declarar la linea donde se va a almacenar lo leido por entrada estandar
-    string linea;
-    int contador = 0;
+    // En este struct creado, asignarle el atributo "lineas" con la direccion de memoria de la cola
+    info.lineas = &lineasSTD;
 
-    cout << "\n\tIngrese el texto. Ctrl+D para finalizar.\n" << endl;
-    while (getline(cin, linea)) {
+    for (int i = 0; i < TOTAL_THREADS; i++) { //Por cada iteración se usa un identificador de hilo
 
-        // Encolar la linea leida por entrada estandar
-        lineasSTD.push(linea);
-        // Aumentar el contador
-        contador++;
-
-        if(contador == 5){
-            
-            cout << "\nLimite de lineas alcanzado, imprimiendo...\n" << endl;
-            // Inicializar el struct con la direccion de memoria de la cola
-            threadData info;
-            // En este struct creado, asignarle el atributo "lineas" con la direccion de memoria de la cola
-            info.lineas = &lineasSTD;
-
-            for (int i = 0; i < TOTAL_THREADS; i++) { //Por cada iteración se usa un identificador de hilo
-            
-                if (pthread_create(&threads[i], NULL, printQueue, &info) != 0) { //Se esta enviando como argumento un struct que adentro tiene la direccion de memoria de la cola
-                    perror("Error creating thread\n");
-                    return 1;
-                }
-
-            }
-
-            //Esperar a que los hilos terminen la ejecución para cerrarlos y terminar el programa
-            for (int i = 0; i < TOTAL_THREADS; i++) {
-                pthread_join(threads[i], NULL);
-            }
-
-            //Reiniciar contador
-            contador = 0;
+        if (pthread_create(&threads[i], NULL, printQueue, &info) != 0) { //Se esta enviando como argumento un struct que adentro tiene la direccion de memoria de la cola
+            perror("Error creating thread\n");
+            return 1;
         }
 
     }
-    
-    // Esperar a que todos los hilos terminen
-    // pthread_mutex_lock(&mtx);
-    // pthread_mutex_unlock(&mtx);
+
+    // Se ejecuta el metodo productor
+    productor();
+
+    // Esperar a que los hilos terminen la ejecución para cerrarlos y terminar el programa
+    for (int i = 0; i < TOTAL_THREADS; ++i) {
+        pthread_cancel(threads[i]);
+        pthread_join(threads[i], NULL);
+    }
 
     pthread_mutex_destroy(&mtx);
+    pthread_cond_destroy(&cond_var);
     return 0;
 }
