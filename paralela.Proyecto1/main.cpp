998da2df758c477456fa720e7dda6b7591cf43ef
Nathalie <nathalie.alfaro@ucr.ac.cr>
2024-05-27 02:01:23 -0600

    ordena alfabeticamente el hash global con mergesort y pasa todas las palabras a minuscula

--- paralela.Proyecto1/main.cpp
+++ paralela.Proyecto1/main.cpp
@@ -7,6 +7,9 @@
 #include <pthread.h>
 #include <unordered_map>
 #include <sstream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -27,6 +30,101 @@ struct threadData {
     Channel<string> *lineas;
     unordered_map<string, int> *wordCount;
 };
+
+// Función para extraer las claves de la tabla hash en un vector
+vector<string> obtenerClaves(const unordered_map<string, int>& wordCount) {
+    vector<string> claves;
+    for (const auto& par : wordCount) {
+        claves.push_back(par.first);
+    }
+    return claves;
+}
+
+
+// Combinar dos subarreglos ordenados en uno solo
+	void merge(vector<string>& A, int izquierda, int divido, int derecha)
+	{
+		// Calcular los tamaños de los dos subarreglos a fusionar
+		int tamano1 = divido - izquierda + 1;
+		int tamano2 = derecha - divido;
+		// Arreglos temporales para almacenar los elementos de los subarreglos
+        vector<string> arreglo1(tamano1), arreglo2(tamano2);
+
+		// Se copian los elementos de los subarreglos originales en los arreglos temporales
+		for (int i = 0; i < tamano1; i++)
+		{
+			arreglo1[i] = A[izquierda + i];
+		} // Fin for
+
+		for (int j = 0; j < tamano2; j++)
+		{
+			arreglo2[j] = A[divido + 1 + j];
+		} // Fin for
+
+		// Fusionar los dos arreglos temporales en el arreglo original
+		int i = 0;
+		int j = 0;
+		int k = izquierda;
+
+		while (i < tamano1 && j < tamano2)
+		{
+			if (arreglo1[i] <= arreglo2[j])
+			{
+				A[k] = arreglo1[i];
+				i++;
+			}
+			else
+			{
+				A[k] = arreglo2[j];
+				j++;
+			} // Fin if
+			k++;
+		} // Fin while
+
+		// Copiar los elementos restantes del primer subarreglo, si hay
+		while (i < tamano1)
+		{
+			A[k] = arreglo1[i];
+			i++;
+			k++;
+		} // Fin while
+
+		// Se copian los elementos restantes del segundo subarreglo, si hay
+		while (j < tamano2)
+		{
+			A[k] = arreglo2[j];
+			j++;
+			k++;
+		} // Fin while
+	} // Fin merge
+
+/* Divide recursivamente el arreglo en subarreglos más pequeños, hasta que cada subarreglo
+	tenga uno o ningún elemento. Luego, combina los subarreglos ordenados de manera consecutiva
+	utilizando la función merge */
+	void mergeRecursivo(vector<string>& A, int inicio, int final)
+	{
+		// Verifica si el índice de inicio es menor que el índice final, indicando que hay elementos en el subarreglo a ordenar
+		if (inicio < final)
+		{
+			// Se calcula el punto medio del subarreglo
+			int divido = inicio + (final - inicio) / 2;
+			// Ordenar la primera mitad del subarreglo
+			mergeRecursivo(A, inicio, divido);
+			// Ordenar la segunda mitad del subarreglo
+			mergeRecursivo(A, divido + 1, final);
+			// Merge para combinar las dos mitades ordenadas del subarreglo
+			merge(A, inicio, divido, final);
+		} // Fin if
+	} // Fin mergeRecursivo
+
+	
+
+    // Algoritmo de ordenamiento Merge Sort
+	void mergesort(vector<string>& A)
+	{
+        mergeRecursivo(A, 0, A.size() - 1);
+	} // Fin mergesort
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void mergeHahs(unordered_map<string, int>& global, unordered_map<string, int> wordCount) {
 
@@ -84,6 +182,7 @@ void productor(Channel<string> *canalStrings) {
     cerr << "\n\tIngrese el texto. Ctrl+D para finalizar.\n" << endl;
 
     while (getline(cin, linea)) {
+        transform(linea.begin(), linea.end(), linea.begin(), ::tolower);
         canalStrings->sendChannel(linea);
     } // End while
 } // End productor
@@ -131,6 +230,15 @@ int main() {
     cout << endl << "Hash table global:\n" << endl;
     for (const auto& pair : global) {
         cout << pair.first << ": " << pair.second << endl;
+    }
+
+    vector<string> claves = obtenerClaves(global);
+    // Ordenar las claves
+    mergesort(claves);
+    // Mostrar las claves ordenadas con sus frecuencias
+    cout << "\n\nPalabras ordenadas alfabéticamente:\n";
+    for (const string& clave : claves) {
+        cout << clave << ": " << global[clave] << "\n";
     }
 
     // Esperar a que los hilos terminen la ejecución para cerrarlos y terminar el programa
