12f440f86fde69579a4d96d4c2c122f08ae9dec9
Nathalie <nathalie.alfaro@ucr.ac.cr>
2024-05-29 23:59:13 -0600

    code tipo POO, clang format, editar readme, documentacion interna, no fugas de memoria con valgrind en el texto actual

--- paralela.Proyecto1/main.cpp
+++ paralela.Proyecto1/main.cpp
@@ -1,254 +1,155 @@
+// Encabezados
+#include "arregloClaves.h"
+#include "channel.h"
+#include "ordenamientoMezcla.h"
+#include "productor.h"
+#include "unirTablas.h"
+
+// Bibliotecas
+#include <algorithm>
+#include <condition_variable>
+#include <cstring>
 #include <iostream>
+#include <mutex>
+#include <pthread.h>
+#include <sstream>
 #include <string>
 #include <thread>
-#include <mutex>
-#include <condition_variable>
-#include "channel.h"
-#include <pthread.h>
+#include <unistd.h>
 #include <unordered_map>
-#include <sstream>
-#include <cstring>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-// Mutex para sincronizar la salida en la consola
+// Mutex para sincronizar la salida en la consola.
 pthread_mutex_t mtx;
 
-// Definir el total de hilos a usar
-#define TOTAL_THREADS 5
+// Definir el total de hilos a usar como el número de núcleos de la CPU
+// disponibles.
+#define TOTAL_THREADS sysconf(_SC_NPROCESSORS_ONLN)
 
-// Variable de condición para notificar a los consumidores
-pthread_cond_t cond_var;
-
+// Mapa global que almacenará el recuento de palabras procesadas por todos los
+// hilos.
 unordered_map<string, int> global;
 
-// Se crea el struct para pasar el argumento a los hilos, el cual es principalmente la cola
+/**
+ * Estructura para pasar los argumentos a los hilos, que principalmente incluye
+ * el canal y el recuento de palabras.
+ */
 struct threadData {
-    //Ya no se usa la cola directamente, sino la clase Chanel (El canal)
-    Channel<string> *lineas;
-    unordered_map<string, int> *wordCount;
+  // Puntero al canal de entrada de texto.
+  Channel<string> *lineas;
+
+  // Puntero al mapa de recuento de palabras del hilo.
+  unordered_map<string, int> *wordCount;
 };
 
-// Función para extraer las claves de la tabla hash en un vector
-vector<string> obtenerClaves(const unordered_map<string, int>& wordCount) {
-    vector<string> claves;
-    for (const auto& par : wordCount) {
-        claves.push_back(par.first);
-    }
-    return claves;
-}
+/**
+ * Función que cuenta las palabras en las líneas recibidas de un canal de
+ * entrada.
+ *
+ * @param threadDataPointer Puntero genérico que apunta a la estructura de datos
+ * del hilo.
+ * @return NULL al finalizar la ejecución del hilo.
+ */
+void *countWords(void *threadDataPointer) {
+  // Recibe el puntero de datos del hilo y lo convierte al tipo correcto
+  struct threadData *data = (struct threadData *)threadDataPointer;
 
+  // Obtiene el canal de entrada y la tabla de conteo de palabras del hilo
+  Channel<string> *lineas = data->lineas;
+  unordered_map<string, int> *wordCount = data->wordCount;
 
-// Combinar dos subarreglos ordenados en uno solo
-	void merge(vector<string>& A, int izquierda, int divido, int derecha)
-	{
-		// Calcular los tamaños de los dos subarreglos a fusionar
-		int tamano1 = divido - izquierda + 1;
-		int tamano2 = derecha - divido;
-		// Arreglos temporales para almacenar los elementos de los subarreglos
-        vector<string> arreglo1(tamano1), arreglo2(tamano2);
+  // Bucle principal del hilo para procesar líneas
+  while (true) {
+    // Bloquea el mutex antes de verificar el canal de entrada
+    pthread_mutex_lock(&mtx);
 
-		// Se copian los elementos de los subarreglos originales en los arreglos temporales
-		for (int i = 0; i < tamano1; i++)
-		{
-			arreglo1[i] = A[izquierda + i];
-		} // Fin for
+    // Verifica si el canal está cerrado y no hay más datos, luego desbloquea el
+    // mutex y sale del bucle
+    if (lineas->isEmpty() && lineas->isClosed()) {
+      // Desbloquea el mutex antes de salir
+      pthread_mutex_unlock(&mtx);
+      break;
+    } // End if
 
-		for (int j = 0; j < tamano2; j++)
-		{
-			arreglo2[j] = A[divido + 1 + j];
-		} // Fin for
+    // Recibe una línea del canal de entrada y luego desbloquea el mutex
+    auto valorGenerico = lineas->receiveChannel();
+    // Desbloquea el mutex después de recibir el valor
+    pthread_mutex_unlock(&mtx);
 
-		// Fusionar los dos arreglos temporales en el arreglo original
-		int i = 0;
-		int j = 0;
-		int k = izquierda;
+    // Procesa la línea recibida
+    string linea = *valorGenerico;
+    istringstream iss(linea);
+    string word;
 
-		while (i < tamano1 && j < tamano2)
-		{
-			if (arreglo1[i] <= arreglo2[j])
-			{
-				A[k] = arreglo1[i];
-				i++;
-			}
-			else
-			{
-				A[k] = arreglo2[j];
-				j++;
-			} // Fin if
-			k++;
-		} // Fin while
-
-		// Copiar los elementos restantes del primer subarreglo, si hay
-		while (i < tamano1)
-		{
-			A[k] = arreglo1[i];
-			i++;
-			k++;
-		} // Fin while
-
-		// Se copian los elementos restantes del segundo subarreglo, si hay
-		while (j < tamano2)
-		{
-			A[k] = arreglo2[j];
-			j++;
-			k++;
-		} // Fin while
-	} // Fin merge
-
-/* Divide recursivamente el arreglo en subarreglos más pequeños, hasta que cada subarreglo
-	tenga uno o ningún elemento. Luego, combina los subarreglos ordenados de manera consecutiva
-	utilizando la función merge */
-	void mergeRecursivo(vector<string>& A, int inicio, int final)
-	{
-		// Verifica si el índice de inicio es menor que el índice final, indicando que hay elementos en el subarreglo a ordenar
-		if (inicio < final)
-		{
-			// Se calcula el punto medio del subarreglo
-			int divido = inicio + (final - inicio) / 2;
-			// Ordenar la primera mitad del subarreglo
-			mergeRecursivo(A, inicio, divido);
-			// Ordenar la segunda mitad del subarreglo
-			mergeRecursivo(A, divido + 1, final);
-			// Merge para combinar las dos mitades ordenadas del subarreglo
-			merge(A, inicio, divido, final);
-		} // Fin if
-	} // Fin mergeRecursivo
-
-	
-
-    // Algoritmo de ordenamiento Merge Sort
-	void mergesort(vector<string>& A)
-	{
-        mergeRecursivo(A, 0, A.size() - 1);
-	} // Fin mergesort
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void mergeHahs(unordered_map<string, int>& global, unordered_map<string, int> wordCount) {
-
-      // Agregar elementos de la tabla local al global
-    for (const auto& pair : wordCount) {
-        //global[pair.first] = pair.second;
-
-        /////// 
-        if (global.find(pair.first) != global.end()) {
-            // Si el elemento está en la primera tabla, incrementar el contador
-           // cout << "adios" << endl;
-            global[pair.first]++;
-        } else {
-           // cout << "holaa" << endl;
-            // Si no está en la primera tabla, agregar el elemento
-            global[pair.first] = pair.second;
-        }
-    }
-}
-
-// ---------------------------------------METODOS-------------------------------------------//
-
-// Metodo de imprimir
-// El parametro de esta funcion es un puntero generico, un puntero generico es un puntero que puede apuntar a cualquier tipo de dato
-void *printQueue(void *threadDataPointer){ 
-
-    // Recibir correctamente el struct y desreferenciarlo
-    struct threadData *data = (struct threadData *)threadDataPointer;
-
-    // Definir un canal de stringsm ya no es una cola
-    Channel<string> *lineas = data->lineas;
-    unordered_map<string, int> *wordCount = data->wordCount;
-
-    while (true) {
-
-        //Uso de "auto" porque en receiveChannel() devuelve un optional<T>
-        auto valorGenerico = lineas->receiveChannel();
-        string linea = *valorGenerico;
-      //  cout << "\n\nProcesando línea: " << linea << endl;
-        
-        istringstream iss(linea);
-        string word;
-
-        while (iss >> word) {
-            (*wordCount)[word]++;
-        } // End while
-
+    // Divide la línea en palabras y las cuenta
+    while (iss >> word) {
+      (*wordCount)[word]++;
     } // End while
-    pthread_exit(NULL);
-} // End printQueue
+  }   // End while
+  // Finaliza la ejecución del hilo y devuelve NULL
+  pthread_exit(NULL);
+} // End countWords
 
-// Metodo productor
-void productor(Channel<string> *canalStrings) {
-    string linea;
-    cerr << "\n\tIngrese el texto. Ctrl+D para finalizar.\n" << endl;
-
-    while (getline(cin, linea)) {
-        transform(linea.begin(), linea.end(), linea.begin(), ::tolower);
-        canalStrings->sendChannel(linea);
-    } // End while
-} // End productor
-
-// -----------------------------------------MAIN--------------------------------------------//
 int main() {
+  // Crear instancias:
+  ordenamientoMezcla ordenamientoMezcla;
+  productor productor;
+  unirTablas unirTablas;
+  arregloClaves arregloClaves;
 
-    // Definir cuantos hilos se van a usar, una constante o una variable que define cuántos hilos se van a crear
-    pthread_t threads[TOTAL_THREADS];
+  // Definir el número de hilos a utilizar según la cantidad de núcleos de la
+  // CPU disponibles
+  pthread_t threads[TOTAL_THREADS];
 
-    // Inicialización de Mutex para evitar errores de sincronización
-    pthread_mutex_init(&mtx, NULL);
+  // Inicialización del Mutex para evitar errores de sincronización
+  pthread_mutex_init(&mtx, NULL);
 
-    // Inicializar el canal
-    Channel<string> canalStrings;
+  // Inicialización del canal de entrada de texto
+  Channel<string> canalStrings;
 
-    threadData info[TOTAL_THREADS];
-    unordered_map<string, int> wordCounts[TOTAL_THREADS];
+  // Arreglo de estructuras para almacenar los datos de cada hilo
+  threadData info[TOTAL_THREADS];
 
-    for (int i = 0; i < TOTAL_THREADS; i++) {
-        info[i].lineas = &canalStrings;
-        info[i].wordCount = &wordCounts[i];
+  // Arreglo de mapas para almacenar el recuento de palabras de cada hilo
+  unordered_map<string, int> wordCounts[TOTAL_THREADS];
 
-        if (pthread_create(&threads[i], NULL, printQueue, &info[i]) != 0) {
-            perror("Error creating thread\n");
-            return 1;
-        } // End if
-    } // End for
+  // Creación de hilos para procesar el texto
+  for (int i = 0; i < TOTAL_THREADS; i++) {
+    info[i].lineas = &canalStrings;
+    info[i].wordCount = &wordCounts[i];
 
-    // Se ejecuta el metodo productor, con el canal para lo carguen con los strings
-    productor(&canalStrings);
+    if (pthread_create(&threads[i], NULL, countWords, &info[i]) != 0) {
+      perror("Error creating thread\n");
+      return 1;
+    } // End if
+  }   // End for
 
-    cout << endl;
-     for (int i = 0; i < TOTAL_THREADS; ++i) {
-        cout << "\nContenido de la tabla hash del hilo " << i << ":\n";
-        for (const auto& entry : wordCounts[i]) {
-            cout << entry.first << ": " << entry.second << endl;
-            // la pasamos al hash global
-           // pthread_mutex_lock(&mtx);
-           // pthread_mutex_unlock(&mtx);
-        }
-        mergeHahs(global, wordCounts[i]);
-    }
+  // Ingresar el texto al canal
+  productor.productorFunction(&canalStrings);
 
-    cout << endl << "Hash table global:\n" << endl;
-    for (const auto& pair : global) {
-        cout << pair.first << ": " << pair.second << endl;
-    }
+  // Espera a que todos los hilos terminen su ejecución y fusiona los resultados
+  for (int i = 0; i < TOTAL_THREADS; ++i) {
+    pthread_join(threads[i], NULL);
+    // Fusionar el recuento de palabras del hilo i con el recuento global
+    pthread_mutex_lock(&mtx);
+    unirTablas.mergeHash(global, wordCounts[i]);
+    pthread_mutex_unlock(&mtx);
+  } // End for
 
-    vector<string> claves = obtenerClaves(global);
-    // Ordenar las claves
-    mergesort(claves);
-    // Mostrar las claves ordenadas con sus frecuencias
-    cout << "\n\nPalabras ordenadas alfabéticamente:\n";
-    for (const string& clave : claves) {
-        cout << clave << ": " << global[clave] << "\n";
-    }
+  // Obtener las claves del mapa global
+  vector<string> claves = arregloClaves.obtenerClaves(global);
+  // Ordenar las claves alfabéticamente
+  ordenamientoMezcla.mergesort(claves);
 
-    // Esperar a que los hilos terminen la ejecución para cerrarlos y terminar el programa
-    for (int i = 0; i < TOTAL_THREADS; ++i) {
-        //Quitar este cancel
-        pthread_cancel(threads[i]);
-        pthread_join(threads[i], NULL);
-    }
+  // Mostrar las claves ordenadas con su frecuencia
+  //cerr << "\n\nPalabras ordenadas alfabéticamente:\n";
+  for (const string &clave : claves) {
+    cout << clave << ": " << global[clave] << "\n";
+  } // End for
 
-    pthread_mutex_destroy(&mtx);
-    pthread_cond_destroy(&cond_var);
-    return 0;
-}
+  // Destrucción del Mutex
+  pthread_mutex_destroy(&mtx);
+  return 0;
+} // End main
