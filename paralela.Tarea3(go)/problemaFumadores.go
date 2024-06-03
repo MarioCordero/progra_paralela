036797ea8a14b01873f0d9421e4c1ef6b4d03e06
MarioCordero <mariogabriel.cordero@ucr.ac.cr>
2024-06-03 12:27:50 -0600

    Nunca pude hacer bien este brete necesito ayuda

--- paralela.Tarea3(go)/problemaFumadores.go
+++ paralela.Tarea3(go)/problemaFumadores.go
@@ -9,74 +9,69 @@ import (
 
 // Número de fumadores
 const numSmokers = 3
+const maxIterations = 10 // Número máximo de iteraciones antes de finalizar
 
 // Estructura para representar los ingredientes
 type Ingrediente int
 
+var globalCounter = 0
+
 // Declaración de constantes
 const (
-	tabaco 		Ingrediente = 0
-	papel   	Ingrediente = 1
-	fosforos 	Ingrediente = 2
+	tabaco   Ingrediente = 0
+	papel    Ingrediente = 1
+	fosforos Ingrediente = 2
 )
 
-// Nombres de los ingredientes para imprimir, esta sintaxis se usa para definir que es una lista de cadenas
+// Nombres de los ingredientes para imprimir
 var ingredientNames = []string{
-	"Tabaco", 
-	"Papel", 
-	"Fósforos"}
+	"Tabaco",
+	"Papel",
+	"Fósforos",
+}
 
-// Canales para los ingredientes, se usa el numero definido anteriormente y se crea un array de canales
+// Canales para los ingredientes
 var ingredientChannels [numSmokers]chan struct{}
-//		struct{} indica que el tipo de datos que el canal transporta es una estructura vacía. Las estructuras vacías 
-// (struct{}) en Go no ocupan espacio, lo que las hace útiles para señales y sincronización, donde el valor en sí no 
-// importa, solo el hecho de que se ha enviado algo a través del canal.
 
 // Canal para el agente
-//		make es una función integrada en Go que se utiliza para crear canales, slices y mapas.
-// chan struct{} indica que estamos creando un canal que transportará valores del tipo struct{}.
-var agentChannel = make(
-	chan struct{})
+var agentChannel = make(chan struct{}, 1) // Hacer el canal con buffer
 
 // WaitGroup para esperar a que todos los fumadores terminen
-// 		Este es el tipo de la variable. sync.WaitGroup es una estructura 
-// proporcionada por el paquete sync de Go que permite esperar a que un 
-// conjunto de goroutines terminen su ejecución.
 var wg sync.WaitGroup
 
 // Mutex para la sincronización
 var mutex sync.Mutex
 
-// Estado de los ingredientes en la mesa, este array se usará para saber que ingredientes hay actualmente en el array
+// Estado de los ingredientes en la mesa, empieza con todo en false, diciendo que no hay ningún ingrediente en la mesa
 var onTable = [numSmokers]bool{false, false, false}
 
 // Función del agente
 func agent() {
-	// Se una un for sin condicion porque con el "<-agentChannel" se esta esperando señal
 	for {
-		// Esperar señal para poner ingredientes
 		<-agentChannel
+		fmt.Println("\nAgente recibió señal para poner ingredientes en la mesa.\n")
 
-		// Seleccionar dos ingredientes aleatorios
 		mutex.Lock()
+		if globalCounter >= maxIterations {
+			// Señalar el fin del proceso
+			mutex.Unlock()
+			fmt.Println("Agente ha alcanzado el máximo de iteraciones, finalizando.")
+			return
+		}
+
 		ingredient1 := rand.Intn(numSmokers)
 		ingredient2 := (ingredient1 + rand.Intn(numSmokers-1) + 1) % numSmokers
 
-		// Asegurar que los dos ingredientes sean diferentes
 		if ingredient1 == ingredient2 {
 			ingredient2 = (ingredient2 + 1) % numSmokers
 		}
 
-		// Actualizar el estado de los ingredientes en la mesa, ahora están en la mesa
 		onTable[ingredient1] = true
 		onTable[ingredient2] = true
 
 		fmt.Printf("El agente pone [%s] y [%s] en la mesa.\n", ingredientNames[ingredient1], ingredientNames[ingredient2])
 
-		// Señalar a los fumadores que los ingredientes están en la mesa
-
-		// 		<- struct{}{} es la sintaxis para enviar una señal a un canal en Go. La estructura vacía struct{} se usa comúnmente 
-		// como un tipo de dato para los canales que no necesitan transportar ningún dato, sino simplemente señalar un evento.
+		// Aqui enviamos las señales a los canales
 		ingredientChannels[ingredient1] <- struct{}{}
 		ingredientChannels[ingredient2] <- struct{}{}
 		mutex.Unlock()
@@ -85,22 +80,33 @@ func agent() {
 
 // Función del fumador
 func smoker(id int, has Ingrediente) {
-	// 		defer wg.Done() se utiliza para indicar al WaitGroup wg que esta goroutine ha terminado después de que la función smoker retorne. 
-	// Esto se usa para coordinar la espera de todas las goroutines de los fumadores en la función main.
+	// 		cuando se ejecuta defer wg.Done(), se está indicando que, cuando una goroutine de fumador finalice su trabajo y 
+	// esté a punto de retornar, se debe informar al WaitGroup que esa goroutine ha terminado. Esto decrementa el contador 
+	// interno del WaitGroup.
 	defer wg.Done()
 	for {
-		// Esperar a que los ingredientes estén en la mesa
+		fmt.Printf("Fumador con [%s] esperando ingredientes.\n", ingredientNames[has])
+		// Aqui esperamos, recibimos las señales de los canales
 		<-ingredientChannels[(has+1)%numSmokers]
 		<-ingredientChannels[(has+2)%numSmokers]
+		fmt.Printf("Fumador con [%s] recibió señal para verificar la mesa.\n", ingredientNames[has])
 
 		mutex.Lock()
-		// Verificar si los ingredientes que necesita están en la mesa
+		// Este if verifica sí están los ingretiendes en la mesa, ambos ingredientes deben ser "true" para que el fumador pueda proceder a fumar.
 		if onTable[(has+1)%numSmokers] && onTable[(has+2)%numSmokers] {
-			// Fumar (simulación con sleep)
-			fmt.Printf("Fumador con [%s] está fumando, dejenlo.\n", ingredientNames[has])
-			time.Sleep(time.Second)
+			if globalCounter >= maxIterations {
+				// Señalar el fin del proceso
+				mutex.Unlock()
+				fmt.Printf("Fumador con [%s] ha alcanzado el máximo de iteraciones, finalizando.\n", ingredientNames[has])
+				return
+			}
 
-			// Limpiar la mesa
+			// Iterador para verificar las vueltas que lleva
+			fmt.Printf("\nIterador = %d\n", globalCounter)
+			globalCounter++
+			fmt.Printf("Fumador con [%s] está fumando, dejenlo.\n", ingredientNames[has])
+
+			// Quita los ingredientes de la mesa
 			onTable[(has+1)%numSmokers] = false
 			onTable[(has+2)%numSmokers] = false
 
@@ -112,27 +118,30 @@ func smoker(id int, has Ingrediente) {
 }
 
 func main() {
+	fmt.Printf("\nEmpieza el codigo...\n\n")
+	// Inicializador de rand, para los rand usado en la función de agent
 	rand.Seed(time.Now().UnixNano())
 
-	// Inicializar los canales
+	//		El propósito de este código es inicializar cada canal en el array ingredientChannels con un canal buffered 
+	// de tamaño 1. Esto permite a cada canal ingredientChannels[i] almacenar una señal (un valor de tipo struct{}) 
+	// antes de que un receptor la reciba, lo que es útil para sincronización en concurrencia.
 	for i := range ingredientChannels {
-		ingredientChannels[i] = make(chan struct{}, 1) // Hacer los canales con buffer de tamaño 1
+		ingredientChannels[i] = make(chan struct{}, 1)
 	}
 
-	// Añadir fumadores al WaitGroup
+	//		Cuando se usa wg.Add(numSmokers), se está incrementando el contador interno del WaitGroup (wg) en la cantidad 
+	// de fumadores que hay en el programa. Esto indica que hay varias goroutines que el programa debe esperar a que 
+	// terminen antes de continuar.
 	wg.Add(numSmokers)
 
-	// Iniciar las goroutines de los fumadores
 	for i := 0; i < numSmokers; i++ {
 		go smoker(i, Ingrediente(i))
 	}
 
-	// Iniciar la goroutine del agente
 	go agent()
 
-	// Iniciar el proceso con una señal al agente
 	agentChannel <- struct{}{}
 
-	// Esperar a que los fumadores terminen (nunca sucede en esta simulación)
 	wg.Wait()
+	fmt.Println("Proceso completado")
 }
